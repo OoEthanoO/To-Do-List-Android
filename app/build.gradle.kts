ad06f729543ce1239a8c23df9c6f49945e8da0a2
Ethan Xu <ethanxucoder@gmail.com>
2023-12-01 20:08:22 -0500

    Improve UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -67,5 +67,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("com.google.code.gson:gson:2.8.9")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("androidx.navigation:navigation-compose:2.7.5")
 }
--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -4,13 +4,14 @@ import android.content.*
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -29,106 +30,55 @@ import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.input.key.type
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.todolist.ui.theme.ToDoListTheme
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import androidx.core.view.WindowCompat
+import kotlin.random.Random
 
 data class Task(
+    val id: Int,
     val title: String,
     val isComplete: Boolean = false
 )
 
+private var taskList by mutableStateOf(listOf<Task>())
+
+sealed class Screen(val route: String) {
+    object TaskListScreen : Screen("task_list")
+    data class TaskDetailScreen(val taskId: Int) : Screen("taskDetail/{taskId}") {
+        fun createRoute(taskId: Int): String {
+            return "taskDetail/$taskId"
+        }
+    }
+}
+
 class MainActivity : ComponentActivity() {
     private lateinit var sharedPreferences: SharedPreferences
-    private var taskList by mutableStateOf(listOf<Task>())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, true)
         sharedPreferences = getSharedPreferences("ToDoList", MODE_PRIVATE)
         taskList = loadTasks()
 
         setContent {
-            ToDoListTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    ToDoContent()
-                }
-            }
-        }
-    }
-
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun ToDoContent(modifier: Modifier = Modifier) {
-        var textValue by remember { mutableStateOf("") }
-
-        Column(modifier = modifier.fillMaxSize()) {
-            LazyColumn(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(16.dp)
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
             ) {
-                items(taskList.size) { index ->
-                    TaskRow(
-                        task = taskList[index],
-                        onDelete = { deleteTask(index) },
-                        onCompleteToggle = { toggleCompleteTask(index) },
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    if (index < taskList.size - 1) {
-                        Divider()
-                    }
-                }
+                Navigation()
             }
-
-            TextField(
-                value = textValue,
-                onValueChange = { textValue = it },
-                label = { Text("Add new task") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .onKeyEvent { it ->
-                        if (it.key == Key.Enter && it.type == KeyEventType.KeyUp) {
-                            if (textValue.isNotBlank()) {
-                                val newTask = Task(textValue.trimEnd { it == '\n' })
-                                taskList = taskList + newTask
-                                textValue = ""
-                                saveTasks()
-                            }
-                            true
-                        } else {
-                            false
-                        }
-                    },
-                keyboardActions = KeyboardActions(
-                    onDone = {
-                        if (textValue.isNotBlank()) {
-                            val newTask = Task(textValue)
-                            taskList = taskList + newTask
-                            textValue = ""
-                            saveTasks()
-                        }
-                    }
-                ),
-                singleLine = true
-            )
         }
-    }
-
-    private fun saveTasks() {
-        val jsonTasks = Gson().toJson(taskList)
-        sharedPreferences.edit().apply {
-            putString("taskList", jsonTasks)
-        }.apply()
     }
 
     private fun loadTasks(): List<Task> {
@@ -152,40 +102,143 @@ class MainActivity : ComponentActivity() {
         }
         saveTasks()
     }
-}
 
-@Composable
-fun TaskRow(task: Task, onDelete: () -> Unit, onCompleteToggle: () -> Unit, modifier: Modifier) {
-    Row(
-        modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        IconButton(onClick = onCompleteToggle) {
-            if (task.isComplete) {
-                Icon(Icons.Default.CheckCircle, contentDescription = "Complete", tint = Color.Green)
-            } else {
-                Icon(Icons.Default.Check, contentDescription = "Incomplete", tint = Color.Blue)
+    private fun saveTasks() {
+        val jsonTasks = Gson().toJson(taskList)
+        sharedPreferences.edit().apply {
+            putString("taskList", jsonTasks)
+        }.apply()
+    }
+
+    @Composable
+    fun Navigation() {
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = Screen.TaskListScreen.route) {
+            composable(Screen.TaskListScreen.route) {
+                TaskListScreen(navController = navController, tasks = taskList, onDelete = ::deleteTask, onCompleteToggle = ::toggleCompleteTask)
+            }
+            composable("taskDetail/{taskId}") { backStackEntry ->
+                val taskId = backStackEntry.arguments?.getString("taskId")?.toIntOrNull()
+                val task = taskList.find { it.id == taskId }
+                TaskDetailScreen(navController = navController, task = task)
             }
         }
-        Text(
-            text = buildAnnotatedString { 
-                withStyle(style = SpanStyle(textDecoration = if (task.isComplete) TextDecoration.LineThrough else TextDecoration.None)) {
-                    append(task.title)
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun TaskListScreen(navController: NavHostController, tasks: List<Task>, onDelete: (Int) -> Unit, onCompleteToggle: (Int) -> Unit) {
+        var textValue by remember { mutableStateOf("") }
+        Column(
+            modifier = Modifier.fillMaxSize())
+        {
+            Text(
+                text = "To Do List",
+                modifier = Modifier.padding(16.dp),
+                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 36.sp)
+            )
+
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(16.dp)
+            ) {
+                items(tasks.size) { index ->
+                    TaskRow(
+                        task = tasks[index],
+                        onDelete = { onDelete(index) },
+                        onCompleteToggle = { onCompleteToggle(index) },
+                        onClick = { navController.navigate(Screen.TaskDetailScreen(tasks[index].id).createRoute(tasks[index].id)) },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    if (index < tasks.size - 1) {
+                        Divider()
+                    }
                 }
-            },
-            color = if (task.isComplete) Color.Gray else Color.Black
-        )
-        IconButton(onClick = onDelete) {
-            Icon(Icons.Default.Delete, contentDescription = "Delete")
+            }
+
+            TextField(
+                value = textValue,
+                onValueChange = { textValue = it },
+                label = { Text("Add new task") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .onKeyEvent { it ->
+                        if (it.key == Key.Enter && it.type == KeyEventType.KeyUp) {
+                            if (textValue.isNotBlank()) {
+                                val newTask = Task(id = Random.nextInt(), textValue.trimEnd { it == '\n' })
+                                taskList = taskList + newTask
+                                textValue = ""
+                                saveTasks()
+                            }
+                            true
+                        } else {
+                            false
+                        }
+                    },
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        if (textValue.isNotBlank()) {
+                            val newTask = Task(id = Random.nextInt(), textValue)
+                            taskList = taskList + newTask
+                            textValue = ""
+                            saveTasks()
+                        }
+                    }
+                ),
+                singleLine = true
+            )
         }
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ToDoListTheme {
-        MainActivity().ToDoContent()
+    @Composable
+    fun TaskDetailScreen(navController: NavHostController, task: Task?) {
+        Column {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(Icons.Default.ArrowBack, contentDescription = "Go back")
+                }
+                Text("To Do List", style = TextStyle(fontSize = 18.sp))
+            }
+            Text(
+                "Edit Task",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 32.sp)
+            )
+            if (task != null) {
+                Text(
+                    task.title,
+                    modifier = Modifier.padding(50.dp),
+                    style = TextStyle(fontSize = 18.sp)
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun TaskRow(task: Task, onDelete: () -> Unit, onCompleteToggle: () -> Unit, onClick: () -> Unit, modifier: Modifier = Modifier) {
+         Row(
+            modifier = modifier.clickable(onClick = onClick),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Checkbox(
+                checked = task.isComplete,
+                onCheckedChange = { onCompleteToggle() }
+            )
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(style = SpanStyle(textDecoration = if (task.isComplete) TextDecoration.LineThrough else TextDecoration.None)) {
+                        append(task.title)
+                    }
+                },
+                color = if (task.isComplete) Color.Gray else Color.Black
+            )
+            IconButton(onClick = onDelete) {
+                Icon(Icons.Default.Delete, contentDescription = "Delete", tint = Color.Red)
+            }
+        }
+
     }
 }
